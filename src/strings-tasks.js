256a3fb854cae34f385762dac256ba0211f8b186
Viktoriiap <viktoriiap0205199@gmail.com>
2024-12-14 20:11:05 +0100

    feat: solve 10 tasks more

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -159,7 +159,14 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
 function removeFirstOccurrences(str, value) {
-  ;
+  const valueCutPrev = str.indexOf(value);
+  let finalStr = str;
+  if (valueCutPrev > -1) {
+    const valueCutFin = str.indexOf(value) + value.length;
+    const word = str.slice(valueCutPrev, valueCutFin);
+    finalStr = str.replace(word, '');
+  }
+  return finalStr;
 }
 
 /**
@@ -174,8 +181,16 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const valueCutPrev = str.lastIndexOf(value);
+  let finalStr = str;
+  if (valueCutPrev > -1) {
+    const valueCutFin = str.lastIndexOf(value) + value.length;
+    const firstPardFrase = str.slice(0, valueCutPrev);
+    const endPardFrase = str.slice(valueCutFin, value.str);
+    finalStr = firstPardFrase.concat(endPardFrase);
+  }
+  return finalStr;
 }
 
 /**
@@ -190,8 +205,16 @@ function removeLastOccurrences(/* str, value */) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  let counter = 0;
+  let sum = 0;
+  if (str) {
+    for (let i = 0; i < str.length; i += 1) {
+      counter = str.charCodeAt(i);
+      sum += counter;
+    }
+  }
+  return sum;
 }
 
 /**
@@ -205,8 +228,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
